3750638796f4e31b7b3f35f4636359f164fe6c33
Mathew Mani <mathew.mani@ymail.com>
2021-06-11 09:11:23 -0500

    Another Commit

--- src/main/java/com/myapp/users/domain/UserRepository.java
+++ src/main/java/com/myapp/users/domain/UserRepository.java
@@ -1,6 +1,6 @@
 package com.myapp.users.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
-
+//Just a comment
 public interface UserRepository extends CrudRepository<User, Long>{
 }
